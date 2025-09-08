faa7dab0e8d78b96b64ad99f9327cca3a1bd62c2
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2025-09-08 16:32:22 -0500

    Comment out because of weird midas issue

--- BwRegulator.scala
+++ BwRegulator.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.regmapper._
-import midas.targetutils.SynthesizePrintf
+//import midas.targetutils.SynthesizePrintf
 import org.chipsalliance.cde.config.{Parameters, Field, Config}
 
 import freechips.rocketchip.tile.{BRUTileIO, BRUTileAccessIO}
