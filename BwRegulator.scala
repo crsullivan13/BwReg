37966215cd67d8a047a776e9085e39b4110eacda
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2026-01-05 14:30:47 -0600

    Updated all-bank regulator, holes for wb regulation

--- BwRegulator.scala
+++ BwRegulator.scala
@@ -10,45 +10,45 @@ import org.chipsalliance.cde.config.{Parameters, Field, Config}
 
 import freechips.rocketchip.tile.{BRUTileIO, BRUTileAccessIO}
 
-// case class BRUParams (
-//   address: BigInt,
-//   nDomains: Int,
-//   withMonitor: Boolean  // avoid including this when using multiple mempress, too many edges and the regmap gets too large
-// )
+case class BRUParams (
+  address: BigInt,
+  nDomains: Int,
+  withMonitor: Boolean
+)
 
-//case object BRUKey extends Field[Option[BRUParams]](None)
+case object BRUKey extends Field[Option[BRUParams]](None)
 
-class BwRegulator() (implicit p: Parameters) extends LazyModule
+class BwRegulator(params: BRUParams) (implicit p: Parameters) extends LazyModule
 {
   val device = new SimpleDevice("bru",Seq("bru"))
 
   val regnode = new TLRegisterNode(
-    address = Seq(AddressSet(0x20000000, 0x7ff)),
+    address = Seq(AddressSet(params.address, 0x7ff)),
     device = device,
     beatBytes = 8)
 
   val adapterNode = TLAdapterNode()
-  val ioNode = Seq.fill(4)(BundleBridgeSource(() => new BRUTileIO(p(SubsystemBankedCoherenceKey).nBanks)))
-  val coreAccessNode = Seq.fill(4)(BundleBridgeSink[BRUTileAccessIO](Some(() => Flipped(new BRUTileAccessIO(p(SubsystemBankedCoherenceKey).nBanks)))))
-  lazy val module = new BwRegulatorModule(this)
+  val ioNode = Seq.fill(params.nDomains)(BundleBridgeSource(() => new BRUTileIO(p(SubsystemBankedCoherenceKey).nBanks)))
+  // val coreAccessNode = Seq.fill(4)(BundleBridgeSink[BRUTileAccessIO](Some(() => Flipped(new BRUTileAccessIO(p(SubsystemBankedCoherenceKey).nBanks)))))
+  lazy val module = new BwRegulatorModule(this, params)
 }
 
-class BwRegulatorModule(outer: BwRegulator) extends LazyModuleImp(outer)
+class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModuleImp(outer)
 {
   // A TLAdapterNode has equal number of input and output edges
   val n = outer.adapterNode.in.length
   println(s"Number of edges into BRU: $n")
 
-  val nDomains = 4
+  val nDomains = params.nDomains
   require(nDomains <= 32) //Limit for regmapper addresses
 
-  val withMonitor = false
+  val withMonitor = params.withMonitor
 
   val nBanks = p(SubsystemBankedCoherenceKey).nBanks
   val numBankBits = log2Ceil(nBanks)
 
   val throttleIO = outer.ioNode.map(_.bundle)
-  val accessIO = outer.coreAccessNode.map(_.bundle)
+  // val accessIO = outer.coreAccessNode.map(_.bundle)
 
   val memBase = p(ExtMem).get.master.base.U
   val wPeriod = 25 // for max 33.5ms period, F = 1GHz
@@ -59,7 +59,7 @@ class BwRegulatorModule(outer: BwRegulator) extends LazyModuleImp(outer)
   val countInstFetch = RegInit(true.B)
   val periodCntr = Reg(UInt(wPeriod.W))
   val periodLen = Reg(UInt(wPeriod.W))
-  val bankReadCntrs = Seq.fill(nDomains)(RegInit(VecInit(Seq.fill(nBanks)(0.U(w.W)))))
+  val readCntrs = Seq.fill(nDomains)(RegInit(0.U(w.W)))
   val maxReads = Reg(Vec(nDomains, UInt(w.W)))
   // val bankWriteCntrs = Seq.fill(nDomains)(RegInit(VecInit(Seq.fill(nBanks)(0.U(w.W)))))
   // val maxWrites = Reg(Vec(nDomains, UInt(w.W)))
@@ -68,7 +68,7 @@ class BwRegulatorModule(outer: BwRegulator) extends LazyModuleImp(outer)
   val coreAcquireActive = Wire(Vec(n, Bool()))
   // val coreReleaseActive = Wire(Vec(n, Bool()))
   val doesAccessBank = Seq.fill(n)(Wire(Vec(nBanks, Bool())))
-  val throttleReadDomainBanks = RegInit(VecInit(Seq.fill(nDomains)(VecInit(Seq.fill(nBanks)(false.B)))))
+  val throttleReadDomain = RegInit(VecInit(Seq.fill(nDomains)(false.B)))
   // val throttleWriteDomainBanks = RegInit(VecInit(Seq.fill(nDomains)(VecInit(Seq.fill(nBanks)(false.B)))))
 
   val perfEnable = RegInit(false.B)
@@ -82,24 +82,22 @@ class BwRegulatorModule(outer: BwRegulator) extends LazyModuleImp(outer)
 
   // generator loop for domains
   for (i <- 0 until nDomains) {
-    for (j <- 0 until nBanks) {
       // bit vectors for clients that are enabled & access mem in the current cycle & are assigned to domain i & are in accssessing bank j
-      val clientAcquireActBankMasked = (clientDomainIds zip (coreAcquireActive zip doesAccessBank)).map { case (d, (act, bank)) => d === i.U && act && bank(j) }
+      val clientAcquireActMasked = (clientDomainIds zip coreAcquireActive).map { case (d, act) => d === i.U && act }
       // val clientReleaseActBankMasked = (clientDomainIds zip (coreReleaseActive zip doesAccessBank)).map { case (d, (act, bank)) => d === i.U && act && bank(j) }
 
       // should be able to reduce or the masks as sytem bus only allows one request per cycle
-      val shouldIncAcquire = clientAcquireActBankMasked.reduce(_||_)
-      val nextCntAcquire = Mux(enBRUGlobal, Mux(!periodCntrReset, bankReadCntrs(i)(j) + shouldIncAcquire, shouldIncAcquire), 0.U)
+      val shouldIncAcquire = clientAcquireActMasked.reduce(_||_)
+      val nextCntAcquire = Mux(enBRUGlobal, Mux(!periodCntrReset, readCntrs(i) + shouldIncAcquire, shouldIncAcquire), 0.U)
 
-      bankReadCntrs(i)(j) := nextCntAcquire
-      throttleReadDomainBanks(i)(j) := Mux(!enBRUGlobal || periodCntrReset, false.B, nextCntAcquire >= maxReads(i))
+      readCntrs(i) := nextCntAcquire
+      throttleReadDomain(i) := Mux(!enBRUGlobal || periodCntrReset, false.B, nextCntAcquire >= maxReads(i))
 
       // val shouldIncRelease = clientReleaseActBankMasked.reduce(_||_)
       // val nextCntRelease = Mux(enBRUGlobal, Mux(!periodCntrReset, bankWriteCntrs(i)(j) + shouldIncRelease, shouldIncRelease), 0.U)
 
       // bankWriteCntrs(i)(j) := nextCntRelease
       // throttleWriteDomainBanks(i)(j) := Mux(!enBRUGlobal || periodCntrReset, false.B, nextCntRelease >= maxWrites(i))
-    }
   }
 
   //generator loop for client edges
@@ -114,7 +112,7 @@ class BwRegulatorModule(outer: BwRegulator) extends LazyModuleImp(outer)
     val aIsRead = aIsAcquire || (aIsInstFetch && countInstFetch)
 
     coreAcquireActive(i) := clientRegEnable(i) && out.a.fire && aIsRead
-    //coreReleaseActive(i) := clientRegEnable(i) && edge_out.done(out.c) && cIsWb
+    //coreReleaseActive(i) := clientRegEnable(i) && edge_out.done(out.c) && cIsWb..
 
     //per bank support
     //do we access bank j
@@ -145,12 +143,10 @@ class BwRegulatorModule(outer: BwRegulator) extends LazyModuleImp(outer)
     }
 
     when (enBRUGlobal && clientRegEnable(i)) {
-      for (j <- 0 until nBanks ) {
-        when ( ( throttleReadDomainBanks(clientDomainIds(i))(j) && doesAccessBank(i)(j) ) && aIsRead ) {
+        when ( throttleReadDomain(clientDomainIds(i)) && aIsRead ) {
            out.a.valid := false.B
            in.a.ready := false.B
         }
-      }
     }
 
     // Hacky solution to handle different client types, better way to do this?
@@ -245,6 +241,10 @@ trait CanHaveBRU { this: BaseSubsystem =>
     }
 }
 
+class WithBRU(address: BigInt = 0x20000000L, nDomains: Int = 4, withMonitor: Boolean = false) extends Config((_, _, _) => {
+  case BRUKey => Some(BRUParams(address = address, nDomains = nDomains, withMonitor = withMonitor))
+})
+
 // MMIO map examples (nDomains=4, n=4, base 0x00, 8-byte stride):
 // Core regs:
 //   enBRUGlobal=0x00, countInstrFetch=0x08, periodLen=0x10
@@ -253,24 +253,3 @@ trait CanHaveBRU { this: BaseSubsystem =>
 // Counters (withMonitor=true):
 //   nBanks=2: bankCountR[0..7]=0x88..0xC0, bankCountW[0..7]=0xC8..0x100
 //   nBanks=4: bankCountR[0..15]=0x88..0x100, bankCountW[0..15]=0x108..0x180
-
-// trait CanHavePeripheryBRU { this: BaseSubsystem =>
-//   private val portName = "llc-bru"
-
-//   val BwRegulator = p(BRUKey) match {
-//     case Some(params) => {
-//       val BwRegulator = LazyModule(new BwRegulator(params, "llc")(p))
-
-//       pbus.coupleTo(portName) { 
-//         BwRegulator.regnode := 
-//         TLFragmenter(pbus.beatBytes, pbus.blockBytes) := _ }
-
-//       Some(BwRegulator)
-//     }
-//     case None => None
-//   }
-// }
-
-// class WithBRU(address: BigInt = 0x20000000L, nDomains: Int = 4, withMonitor: Boolean = false) extends Config((_, _, _) => {
-//   case BRUKey => Some(BRUParams(address = address, nDomains = nDomains, withMonitor = withMonitor))
-// })
