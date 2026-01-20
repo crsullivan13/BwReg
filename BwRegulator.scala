6e921d623863b5b18ef315bd36a61a04c86a715b
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2026-01-20 17:04:12 -0600

    mmio interface

--- BwRegulator.scala
+++ BwRegulator.scala
@@ -10,10 +10,27 @@ import org.chipsalliance.cde.config.{Parameters, Field, Config}
 
 import freechips.rocketchip.tile.{BRUTileIO, BRUTileAccessIO}
 
+class CapabilitiesBundle() extends Bundle {
+  val ver = UInt(8.W) // [7:0]
+  val nbwblks = UInt(16.W) // [23:8]
+  val rpfx = Bool() // [24]
+  val p = UInt(4.W) // [28:25]
+  val reserved_1 = UInt(3.W) // [31:29]
+  val mrbwb = UInt(16.W) // [47:32]
+  val reserved_2 = UInt(16.W) // [63:48]
+}
+
 case class BRUParams (
   address: BigInt,
   nDomains: Int,
-  withMonitor: Boolean
+  withMonitor: Boolean,
+
+  // bc_capabilities
+  ver: Int,
+  nbwblks: Int,
+  rpfx: Boolean,
+  p: Int,
+  mrbwb: Int
 )
 
 case object BRUKey extends Field[Option[BRUParams]](None)
@@ -72,6 +89,130 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
   val doesAccessBank = Seq.fill(n)(Wire(Vec(nBanks, Bool())))
   val throttleReadDomainBanks = RegInit(VecInit(Seq.fill(nDomains)(VecInit(Seq.fill(nBanks)(false.B)))))
   // val throttleWriteDomainBanks = RegInit(VecInit(Seq.fill(nDomains)(VecInit(Seq.fill(nBanks)(false.B)))))
+
+  val bc_capabilities = WireDefault(0.U.asTypeOf(new CapabilitiesBundle))
+  bc_capabilities.ver := params.ver.U
+  bc_capabilities.nbwblks := params.nbwblks.U
+  bc_capabilities.rpfx := params.rpfx.B
+  bc_capabilities.p := params.p.U
+  bc_capabilities.mrbwb := params.mrbwb.U
+
+  // pack the fields explicitly for regmap
+  val bc_capabilities_u64 = Cat(bc_capabilities.reserved_2,
+    bc_capabilities.mrbwb,
+    bc_capabilities.reserved_1,
+    bc_capabilities.p,
+    bc_capabilities.rpfx,
+    bc_capabilities.nbwblks,
+    bc_capabilities.ver)
+
+  val bc_mon_ctl_op = RegInit(0.U(5.W))                 // [4:0]
+  val bc_mon_ctl_at = RegInit(0.U(3.W))                 // [7:5]
+  val bc_mon_ctl_mcid = RegInit(0.U(12.W))              // [19:8]
+  val bc_mon_ctl_evtid = RegInit(0.U(8.W))              // [27:20]
+  val bc_mon_ctl_atv = RegInit(false.B)                 // [28]
+  val bc_mon_ctl_reserved_1 = WireDefault(0.U(3.W))     // [31:29]
+  val bc_mon_ctl_status = RegInit(0.U(7.W))             // [38:32]
+  val bc_mon_ctl_busy = RegInit(false.B)                // [39]
+  val bc_mon_ctl_reserved_2 = WireDefault(0.U(24.W))    // [63:40]
+
+  val bc_mon_ctl_u64 = Cat(bc_mon_ctl_reserved_2,
+    bc_mon_ctl_busy,
+    bc_mon_ctl_status,
+    bc_mon_ctl_reserved_1,
+    bc_mon_ctl_atv,
+    bc_mon_ctl_evtid,
+    bc_mon_ctl_mcid,
+    bc_mon_ctl_at,
+    bc_mon_ctl_op)
+
+  def writeBcMonCtl(valid: Bool, data: UInt): Bool = {
+    when ( valid && !bc_mon_ctl_busy ) {
+      bc_mon_ctl_op := data(4,0)
+      bc_mon_ctl_at := data(7,5)
+      bc_mon_ctl_mcid := data(19,8)
+      bc_mon_ctl_evtid := data(27,20)
+      bc_mon_ctl_atv := data(28)
+      bc_mon_ctl_busy := true.B
+      bc_mon_ctl_status := 0.U 
+    }
+
+    !bc_mon_ctl_busy
+  }
+
+  def readBcMonCtl(ready: Bool): (Bool, UInt) = {
+    (true.B, bc_mon_ctl_u64(31,0))
+  }
+
+  val bc_mon_ctr_val = RegInit(0.U(62.W)) // [61:0]
+  val bc_mon_ctr_inv = RegInit(false.B)   // [62]
+  val bc_mon_ctr_ovf = RegInit(false.B)   // [63]
+
+  val bc_mon_ctr_u64 = Cat(bc_mon_ctr_ovf,
+    bc_mon_ctr_inv,
+    bc_mon_ctr_val)
+
+
+  val bc_alloc_ctl_op = RegInit(0.U(5.W))              // [4:0]
+  val bc_alloc_ctl_at = RegInit(0.U(3.W))              // [7:5]
+  val bc_alloc_ctl_rcid = RegInit(0.U(12.W))           // [19:8]
+  val bc_alloc_ctl_reserved_1 = WireDefault(0.U(12.W)) // [31:20] 
+  val bc_alloc_ctl_status = RegInit(0.U(7.W))          // [38:32]
+  val bc_alloc_ctl_busy = RegInit(false.B)             // [39]
+  val bc_alloc_ctl_reserved_2 = WireDefault(0.U(24.W)) // [63:40]
+
+  val bc_alloc_ctl_u64 = Cat(bc_alloc_ctl_reserved_2,
+    bc_alloc_ctl_busy,
+    bc_alloc_ctl_status,
+    bc_alloc_ctl_reserved_1,
+    bc_alloc_ctl_rcid,
+    bc_alloc_ctl_at,
+    bc_alloc_ctl_op)
+
+  def writeBcAllocCtl(valid: Bool, data: UInt): Bool = {
+    when ( valid && !bc_alloc_ctl_busy ) {
+      bc_alloc_ctl_op := data(4,0)
+      bc_alloc_ctl_at := data(7,5)
+      bc_alloc_ctl_rcid := data(19,8)
+      bc_alloc_ctl_status := 0.U
+      bc_alloc_ctl_busy := true.B
+    }
+
+    !bc_alloc_ctl_busy
+  }
+
+  def readBcAllocCtl(ready: Bool): (Bool, UInt) = {
+    (true.B, bc_alloc_ctl_u64(31,0))
+  }
+
+  val bc_bw_alloc_rbwb = RegInit(0.U(16.W))          // [15:0]
+  val bc_bw_alloc_reserved_1 = WireDefault(0.U(4.W)) // [19:16]
+  val bc_bw_alloc_mweight = RegInit(0.U(18.W))       // [27:20]
+  val bc_bw_alloc_shared_at = RegInit(0.U(3.W))      // [30:28]
+  val bc_bw_alloc_use_shared = RegInit(false.B)      // [31]
+  val bc_bw_alloc_reserved_2 = RegInit(0.U(32.W))    // [63:32]
+
+  val bc_bw_alloc_u64 = Cat(bc_bw_alloc_reserved_2,
+    bc_bw_alloc_use_shared,
+    bc_bw_alloc_shared_at,
+    bc_bw_alloc_mweight,
+    bc_bw_alloc_reserved_1,
+    bc_bw_alloc_rbwb)
+
+  def writeBcBwAlloc(valid: Bool, data: UInt): Bool = {
+    when ( valid ) {
+      bc_bw_alloc_rbwb := bc_bw_alloc_u64(15,0)
+      bc_bw_alloc_mweight := bc_bw_alloc_u64(27,20)
+      bc_bw_alloc_shared_at := bc_bw_alloc_u64(30,28)
+      bc_bw_alloc_use_shared := bc_bw_alloc_u64(31)
+    }
+
+    !bc_alloc_ctl_busy
+  }
+
+  def readBcBwAlloc(ready: Bool): (Bool, UInt) = {
+    (true.B, bc_bw_alloc_u64(31,0))
+  }
 
   val perfEnable = RegInit(false.B)
   // It is not required to reset these counters but we keep it for now as it helps to close timing
@@ -165,65 +306,44 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
     }
   }
 
-  val enBRUGlobalRegField = Seq(0 -> Seq(
-    RegField(enBRUGlobal.getWidth, enBRUGlobal,
-      RegFieldDesc("enBRUGlobal", "Enable BRU Global"))))
 
-    val settingsRegField = Seq(8 -> Seq (
-        RegField(countInstFetch.getWidth, countInstFetch,
-            RegFieldDesc("countInstrFetch", "Toggle fetch counting"))))
+  val bc_capabilities_lo32 = RegField.r(32, bc_capabilities_u64(31,0),
+    RegFieldDesc("bc_capabilities_lo32", "Low 32 bits of bc_capabilities"))
+  
+  val bc_capabilities_hi32 = RegField.r(32, bc_capabilities_u64(63,32),
+    RegFieldDesc("bc_capabilities_hi32", "High 32 bits of bc_capabilities"))
 
-    val periodLenRegField = Seq(16 -> Seq (
-        RegField(periodLen.getWidth, periodLen,
-            RegFieldDesc("periodLen", "Set regulation period"))))
+  val bc_mon_ctl_lo32 = RegField(32, readBcMonCtl(_), writeBcMonCtl(_,_), 
+    RegFieldDesc("bc_mon_ctl_lo32", "Writeable portion of bc_mon_ctl"))
 
-    val maxReadsRegField = maxReads.zipWithIndex.map { case (register, i) =>
-        (24 + i * 8) -> Seq(RegField(register.getWidth, register,
-            RegFieldDesc(s"maxRead$i", s"Max reads for domain $i")))}
+  val bc_mon_ctl_hi32 = RegField.r(32, bc_mon_ctl_u64(63,32),
+    RegFieldDesc("bc_mon_ctl_hi32", "Status and busy bits of bc_mon_ctl"))
 
-    // val maxWritesRegField = maxWrites.zipWithIndex.map { case (register, i) =>
-    //     (24 + nDomains * 8 + i * 8) -> Seq(RegField(register.getWidth, register,
-    //         RegFieldDesc(s"maxWrite$i", s"Max writes for domain $i")))}
+  val bc_mon_ctr_lo32 = RegField.r(32, bc_mon_ctr_u64(31,0),
+    RegFieldDesc("bc_mon_ctr_lo32", "Low 32 bits of bc_mon_ctr"))
 
-    val clientRegEnableRegField = Seq((24 + 2 * nDomains * 8) -> clientRegEnable.zipWithIndex.map { case (client, i) =>
-        RegField(client.getWidth, client, RegFieldDesc(s"client${i}En", s"Reg enable for client$i")) })
+  val bc_mon_ctr_hi32 = RegField.r(32, bc_mon_ctr_u64(63,32),
+    RegFieldDesc("bc_mon_ctr_hi32", "High 32 bits of bc_mon_ctr"))
 
-    val domainIdField = clientDomainIds.zipWithIndex.map { case(domain, i) =>
-        (24 + 2 * nDomains * 8 + 8 + i * 8) -> Seq(RegField(domain.getWidth, domain, RegFieldDesc(s"domainId$i", s"Client $i domain ID"))) }
+  val bc_alloc_ctl_lo32 = RegField(32, readBcAllocCtl(_), writeBcAllocCtl(_,_), 
+    RegFieldDesc("bc_alloc_ctl_lo32", "Writeable portion of bc_alloc_ctl"))
 
-  val perfEnField = Seq((24 + 2 * nDomains * 8 + 8 + n * 8) -> Seq(
-    RegField(perfEnable.getWidth, perfEnable,
-      RegFieldDesc("perfEnable", "perfEnable"))))
+  val bc_alloc_ctl_hi32 = RegField.r(32, bc_alloc_ctl_u64(63,32),
+    RegFieldDesc("bc_alloc_ctl_hi32", "Status and busy bits of bc_alloc_ctl"))
 
-  val mmreg = enBRUGlobalRegField ++ settingsRegField ++ periodLenRegField ++ maxReadsRegField ++ clientRegEnableRegField ++
-      domainIdField ++ perfEnField
+  val bc_bw_alloc_lo32 = RegField(32, readBcBwAlloc(_), writeBcBwAlloc(_,_), 
+    RegFieldDesc("bc_bw_alloc_lo32", "Writeable portion of bc_bw_alloc"))
 
-  (aCounters, cCounters) match {
-    case (Some(aCounts), Some(cCounts)) => {
-      val bankReadCountersField = aCounts.zipWithIndex.flatMap { case (banks, i) =>
-        banks.zipWithIndex.map { case (bank, j) =>
-          val addr = 8 * (5 + 2 * nDomains + n + i * nBanks + j)
-          addr -> Seq(
-            RegField(bank.getWidth, bank,
-              RegFieldDesc(s"bankCountR${i * nBanks + j}", s"Bank Read counter"))
-          )
-        }
-      }
+  val bc_bw_alloc_hi32 = RegField.r(32, bc_bw_alloc_u64(63,32),
+    RegFieldDesc("bc_bw_alloc_hi32", "Reserved bits of bc_bw_alloc"))
 
-      val bankWriteCountersField = cCounts.zipWithIndex.flatMap { case (banks, i) =>
-        banks.zipWithIndex.map { case (bank, j) =>
-          val addr = 8 * (5 + 2 * nDomains + n + n * nBanks + i * nBanks + j)
-          addr -> Seq(
-            RegField(bank.getWidth, bank,
-              RegFieldDesc(s"bankCountW${i * nBanks + j}", s"Bank Write counter"))
-          )
-        }
-      }
-
-      outer.regnode.regmap(mmreg ++ bankReadCountersField ++ bankWriteCountersField: _*)
-    }
-    case _ => outer.regnode.regmap(mmreg: _*)
-  }
+  val regmap = outer.regnode.regmap(
+    0x000 -> RegFieldGroup("bc_capabilities", Some("4.1. Bandwidth-controller Capabilities"), Seq(bc_capabilities_lo32, bc_capabilities_hi32)),
+    0x008 -> RegFieldGroup("bc_mon_ctl", Some("4.2. Bandwidth Usage Monitoring Control"), Seq(bc_mon_ctl_lo32, bc_mon_ctl_hi32)),
+    0x010 -> RegFieldGroup("bc_mon_ctr", Some("4.3. Bandwidth Monitoring Counter Value"), Seq(bc_mon_ctr_lo32, bc_mon_ctr_hi32)),
+    0x018 -> RegFieldGroup("bc_alloc_ctl", Some("4.4. Bandwidth Allocation Control"), Seq(bc_alloc_ctl_lo32, bc_alloc_ctl_hi32)),
+    0x020 -> RegFieldGroup("bc_bw_alloc", Some("4.5. Bandwidth Allocation Configuration"), Seq(bc_bw_alloc_lo32, bc_bw_alloc_hi32))
+  )
 
   println("Bandwidth regulation (BRU):")
   for (i <- clientNames.indices)
@@ -247,8 +367,22 @@ trait CanHaveBRU { this: BaseSubsystem =>
     }
 }
 
-class WithBRU(address: BigInt = 0x20000000L, nDomains: Int = 4, withMonitor: Boolean = false) extends Config((_, _, _) => {
-  case BRUKey => Some(BRUParams(address = address, nDomains = nDomains, withMonitor = withMonitor))
+class WithBRU(address: BigInt = 0x20000000L, nDomains: Int = 4, withMonitor: Boolean = false,
+             ver: Int = 1, nbwblks: Int = 65536, rpfx: Boolean = false, p: Int = 0, mrbwb: Int = 65536) 
+extends Config((_, _, _) => {
+  case BRUKey => {
+    Some(BRUParams(
+      address = address, 
+      nDomains = nDomains, 
+      withMonitor = withMonitor,
+
+      ver = ver,
+      nbwblks = nbwblks,
+      rpfx = rpfx,
+      p = p,
+      mrbwb = mrbwb
+    ))
+  }
 })
 
 // MMIO map examples (nDomains=4, n=4, base 0x00, 8-byte stride):
