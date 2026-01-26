9275ab64000143383a87f5996ef14853d4cf7411
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2026-01-26 15:55:18 -0600

    update bw alloc ctl interface

--- BwRegulator.scala
+++ BwRegulator.scala
@@ -10,6 +10,21 @@ import org.chipsalliance.cde.config.{Parameters, Field, Config}
 
 import freechips.rocketchip.tile.{BRUTileIO, BRUTileAccessIO}
 
+object BcAllocCtlStatus extends ChiselEnum {
+  val OK           = Value(1.U)
+  val INVALID_OP   = Value(2.U)
+  val INVALID_RCID = Value(3.U)
+  val INVALID_AT   = Value(4.U)
+  val INVALID_BWB  = Value(5.U) // invalid or unsupported reserved bandwidth block
+}
+
+object BcAllocCtlOp extends ChiselEnum {
+  val CONFIG_LIMIT = Value(1.U)
+  val READ_LIMIT   = Value(2.U)
+
+  val CUSTOM       = Value(31.U) // force this type to be 5 bits wide, this is dumb
+}
+
 class CapabilitiesBundle() extends Bundle {
   val ver = UInt(8.W) // [7:0]
   val nbwblks = UInt(16.W) // [23:8]
@@ -22,7 +37,8 @@ class CapabilitiesBundle() extends Bundle {
 
 case class BRUParams (
   address: BigInt,
-  nDomains: Int,
+  nRCID: Int,
+  nMCID: Int,
   withMonitor: Boolean,
 
   // bc_capabilities
@@ -47,7 +63,7 @@ class BwRegulator(params: BRUParams) (implicit p: Parameters) extends LazyModule
   val adapterNode = TLAdapterNode()
   // NOTE: we currently assume that nDomains == number of cores
   // if you decide to stray from this, change params.nDomains below to be equal to number of cores
-  val ioNode = Seq.fill(params.nDomains)(BundleBridgeSource(() => new BRUTileIO(p(SubsystemBankedCoherenceKey).nBanks)))
+  val ioNode = Seq.fill(4)(BundleBridgeSource(() => new BRUTileIO(p(SubsystemBankedCoherenceKey).nBanks)))
   // val coreAccessNode = Seq.fill(4)(BundleBridgeSink[BRUTileAccessIO](Some(() => Flipped(new BRUTileAccessIO(p(SubsystemBankedCoherenceKey).nBanks)))))
   lazy val module = new BwRegulatorModule(this, params)
 }
@@ -58,8 +74,8 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
   val n = outer.adapterNode.in.length
   println(s"Number of edges into BRU: $n")
 
-  val nDomains = params.nDomains
-  require(nDomains <= 32) //Limit for regmapper addresses
+  val nRCID = params.nRCID
+  val nMCID = params.nMCID
 
   val withMonitor = params.withMonitor
 
@@ -78,17 +94,13 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
   val countInstFetch = RegInit(true.B)
   val periodCntr = Reg(UInt(wPeriod.W))
   val periodLen = Reg(UInt(wPeriod.W))
-  val bankReadCntrs = Seq.fill(nDomains)(RegInit(VecInit(Seq.fill(nBanks)(0.U(w.W)))))
-  val maxReads = Reg(Vec(nDomains, UInt(w.W)))
-  // val bankWriteCntrs = Seq.fill(nDomains)(RegInit(VecInit(Seq.fill(nBanks)(0.U(w.W)))))
-  // val maxWrites = Reg(Vec(nDomains, UInt(w.W)))
+  val bankReadCntrs = Seq.fill(nRCID)(RegInit(VecInit(Seq.fill(nBanks)(0.U(w.W)))))
+  val maxReads = Reg(Vec(nRCID, UInt(w.W)))
   val clientRegEnable = Reg(Vec(n, Bool()))
-  val clientDomainIds = Reg(Vec(n, UInt(log2Ceil(nDomains).W)))
   val coreAcquireActive = Wire(Vec(n, Bool()))
-  // val coreReleaseActive = Wire(Vec(n, Bool()))
+  val coreAcquireRCID = Wire(Vec(n, UInt(log2Ceil(nRCID).W)))
   val doesAccessBank = Seq.fill(n)(Wire(Vec(nBanks, Bool())))
-  val throttleReadDomainBanks = RegInit(VecInit(Seq.fill(nDomains)(VecInit(Seq.fill(nBanks)(false.B)))))
-  // val throttleWriteDomainBanks = RegInit(VecInit(Seq.fill(nDomains)(VecInit(Seq.fill(nBanks)(false.B)))))
+  val throttleReadDomainBanks = RegInit(VecInit(Seq.fill(nRCID)(VecInit(Seq.fill(nBanks)(false.B)))))
 
   val bc_capabilities = WireDefault(0.U.asTypeOf(new CapabilitiesBundle))
   bc_capabilities.ver := params.ver.U
@@ -133,7 +145,7 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
       bc_mon_ctl_mcid := data(19,8)
       bc_mon_ctl_evtid := data(27,20)
       bc_mon_ctl_atv := data(28)
-      bc_mon_ctl_busy := true.B
+      // bc_mon_ctl_busy := true.B
       bc_mon_ctl_status := 0.U 
     }
 
@@ -152,36 +164,60 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
     bc_mon_ctr_inv,
     bc_mon_ctr_val)
 
-
-  val bc_alloc_ctl_op = RegInit(0.U(5.W))              // [4:0]
-  val bc_alloc_ctl_at = RegInit(0.U(3.W))              // [7:5]
-  val bc_alloc_ctl_rcid = RegInit(0.U(12.W))           // [19:8]
-  val bc_alloc_ctl_reserved_1 = WireDefault(0.U(12.W)) // [31:20] 
-  val bc_alloc_ctl_status = RegInit(0.U(7.W))          // [38:32]
-  val bc_alloc_ctl_busy = RegInit(false.B)             // [39]
-  val bc_alloc_ctl_reserved_2 = WireDefault(0.U(24.W)) // [63:40]
+  val bc_alloc_ctl_op = RegInit(BcAllocCtlOp.READ_LIMIT)          // [4:0]
+  val bc_alloc_ctl_at = RegInit(0.U(3.W))                         // [7:5]
+  val bc_alloc_ctl_rcid = RegInit(0.U(12.W))                      // [19:8]
+  val bc_alloc_ctl_reserved_1 = WireDefault(0.U(12.W))            // [31:20] 
+  val bc_alloc_ctl_status = RegInit(BcAllocCtlStatus.OK)          // [38:32]
+  val bc_alloc_ctl_busy = RegInit(false.B)                        // [39]
+  val bc_alloc_ctl_reserved_2 = WireDefault(0.U(24.W))            // [63:40]
 
   val bc_alloc_ctl_u64 = Cat(bc_alloc_ctl_reserved_2,
     bc_alloc_ctl_busy,
-    bc_alloc_ctl_status,
+    bc_alloc_ctl_status.asUInt.pad(7),
     bc_alloc_ctl_reserved_1,
     bc_alloc_ctl_rcid,
     bc_alloc_ctl_at,
-    bc_alloc_ctl_op)
+    bc_alloc_ctl_op.asUInt.pad(5))
+
+  def handleBcAllocCtl(op: UInt, at: UInt, rcid: UInt): Unit = {
+    val opEnum = op.asTypeOf(BcAllocCtlOp())
+    val opValid = (opEnum === BcAllocCtlOp.CONFIG_LIMIT) || (opEnum === BcAllocCtlOp.READ_LIMIT)
+    val rcidValid = rcid >= nRCID.U
+
+    bc_alloc_ctl_op := opEnum
+    bc_alloc_ctl_at := at
+    bc_alloc_ctl_rcid := rcid
+
+    when ( opValid && rcidValid ) {
+      switch ( opEnum ) {
+        is ( BcAllocCtlOp.CONFIG_LIMIT ) {
+          maxReads(rcid) := bc_bw_alloc_rbwb
+          bc_alloc_ctl_status := BcAllocCtlStatus.OK
+        }
+        is ( BcAllocCtlOp.READ_LIMIT ) {
+          bc_bw_alloc_rbwb := maxReads(rcid)
+          bc_alloc_ctl_status := BcAllocCtlStatus.OK
+        }
+      }
+    } .elsewhen ( !rcidValid ) {
+      bc_alloc_ctl_status := BcAllocCtlStatus.INVALID_RCID
+    }.otherwise {
+      bc_alloc_ctl_status := BcAllocCtlStatus.INVALID_OP
+    }
+
+  }
 
   def writeBcAllocCtl(valid: Bool, data: UInt): Bool = {
     when ( valid && !bc_alloc_ctl_busy ) {
-      bc_alloc_ctl_op := data(4,0)
-      bc_alloc_ctl_at := data(7,5)
-      bc_alloc_ctl_rcid := data(19,8)
-      bc_alloc_ctl_status := 0.U
-      bc_alloc_ctl_busy := true.B
+      handleBcAllocCtl(data(4,0), data(7,5), data(19,8))
+      // bc_alloc_ctl_busy := true.B everything should end up being single cycle
     }
 
     !bc_alloc_ctl_busy
   }
 
-  def readBcAllocCtl(ready: Bool): (Bool, UInt) = {
+  def readBcAllocCtlLo32(ready: Bool): (Bool, UInt) = {
     (true.B, bc_alloc_ctl_u64(31,0))
   }
 
@@ -201,10 +237,10 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
 
   def writeBcBwAlloc(valid: Bool, data: UInt): Bool = {
     when ( valid ) {
-      bc_bw_alloc_rbwb := bc_bw_alloc_u64(15,0)
-      bc_bw_alloc_mweight := bc_bw_alloc_u64(27,20)
-      bc_bw_alloc_shared_at := bc_bw_alloc_u64(30,28)
-      bc_bw_alloc_use_shared := bc_bw_alloc_u64(31)
+      bc_bw_alloc_rbwb := data(15,0)
+      // bc_bw_alloc_mweight := data(27,20) ignore these fields for now
+      // bc_bw_alloc_shared_at := data(30,28)
+      // bc_bw_alloc_use_shared := data(31)
     }
 
     !bc_alloc_ctl_busy
@@ -217,18 +253,19 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
   val perfEnable = RegInit(false.B)
   // It is not required to reset these counters but we keep it for now as it helps to close timing
   //  more easily in PnR
-  val aCounters = if ( withMonitor ) Some(Seq.fill(n)(RegInit(VecInit(Seq.fill(nBanks)(0.U(64.W)))))) else None
-  val cCounters = if ( withMonitor ) Some(Seq.fill(n)(RegInit(VecInit(Seq.fill(nBanks)(0.U(64.W)))))) else None
+  val aCounters = if ( withMonitor ) Some(Seq.fill(nMCID)(RegInit(VecInit(Seq.fill(nBanks)(0.U(64.W)))))) else None
+  val cCounters = if ( withMonitor ) Some(Seq.fill(nMCID)(RegInit(VecInit(Seq.fill(nBanks)(0.U(64.W)))))) else None
 
   val periodCntrReset = periodCntr >= periodLen
   periodCntr := Mux(periodCntrReset || !enBRUGlobal, 0.U, periodCntr + 1.U)
 
   // generator loop for domains
-  for (i <- 0 until nDomains) {
+  for (i <- 0 until nRCID) {
     for (j <- 0 until nBanks) {
       // bit vectors for clients that are enabled & access mem in the current cycle & are assigned to domain i & are in accssessing bank j
-      val clientAcquireActBankMasked = (clientDomainIds zip (coreAcquireActive zip doesAccessBank)).map { case (d, (act, bank)) => d === i.U && act && bank(j) }
-      // val clientReleaseActBankMasked = (clientDomainIds zip (coreReleaseActive zip doesAccessBank)).map { case (d, (act, bank)) => d === i.U && act && bank(j) }
+      val clientAcquireActBankMasked = (coreAcquireRCID zip (coreAcquireActive zip doesAccessBank)).map { 
+        case (rcid, (act, bank)) => rcid === i.U && act && bank(j) 
+      }
 
       // should be able to reduce or the masks as sytem bus only allows one request per cycle
       val shouldIncAcquire = clientAcquireActBankMasked.reduce(_||_)
@@ -236,12 +273,6 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
 
       bankReadCntrs(i)(j) := nextCntAcquire
       throttleReadDomainBanks(i)(j) := Mux(!enBRUGlobal || periodCntrReset, false.B, nextCntAcquire >= maxReads(i))
-
-      // val shouldIncRelease = clientReleaseActBankMasked.reduce(_||_)
-      // val nextCntRelease = Mux(enBRUGlobal, Mux(!periodCntrReset, bankWriteCntrs(i)(j) + shouldIncRelease, shouldIncRelease), 0.U)
-
-      // bankWriteCntrs(i)(j) := nextCntRelease
-      // throttleWriteDomainBanks(i)(j) := Mux(!enBRUGlobal || periodCntrReset, false.B, nextCntRelease >= maxWrites(i))
     }
   }
 
@@ -256,8 +287,8 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
 
     val aIsRead = aIsAcquire || (aIsInstFetch && countInstFetch)
 
-    coreAcquireActive(i) := clientRegEnable(i) && out.a.fire && aIsRead
-    //coreReleaseActive(i) := clientRegEnable(i) && edge_out.done(out.c) && cIsWb
+    coreAcquireActive(i) := clientRegEnable(i) && in.a.fire && aIsRead
+    coreAcquireRCID(i) := in.a.bits.rcid
 
     //per bank support
     //do we access bank j
@@ -289,7 +320,7 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
 
     when (enBRUGlobal && clientRegEnable(i)) {
       for (j <- 0 until nBanks ) {
-        when ( ( throttleReadDomainBanks(clientDomainIds(i))(j) && doesAccessBank(i)(j) ) && aIsRead ) {
+        when ( ( throttleReadDomainBanks(coreAcquireRCID(i))(j) && doesAccessBank(i)(j) ) && aIsRead ) {
            out.a.valid := false.B
            in.a.ready := false.B
         }
@@ -325,7 +356,7 @@ class BwRegulatorModule(outer: BwRegulator, params: BRUParams) extends LazyModul
   val bc_mon_ctr_hi32 = RegField.r(32, bc_mon_ctr_u64(63,32),
     RegFieldDesc("bc_mon_ctr_hi32", "High 32 bits of bc_mon_ctr"))
 
-  val bc_alloc_ctl_lo32 = RegField(32, readBcAllocCtl(_), writeBcAllocCtl(_,_), 
+  val bc_alloc_ctl_lo32 = RegField(32, readBcAllocCtlLo32(_), writeBcAllocCtl(_,_), 
     RegFieldDesc("bc_alloc_ctl_lo32", "Writeable portion of bc_alloc_ctl"))
 
   val bc_alloc_ctl_hi32 = RegField.r(32, bc_alloc_ctl_u64(63,32),
@@ -367,13 +398,14 @@ trait CanHaveBRU { this: BaseSubsystem =>
     }
 }
 
-class WithBRU(address: BigInt = 0x20000000L, nDomains: Int = 4, withMonitor: Boolean = false,
-             ver: Int = 1, nbwblks: Int = 65536, rpfx: Boolean = false, p: Int = 0, mrbwb: Int = 65536) 
+class WithBRU(address: BigInt = 0x20000000L, nRCID: Int = 64, nMCID: Int = 64, withMonitor: Boolean = false,
+             ver: Int = 1, nbwblks: Int = 65536, rpfx: Boolean = false, p: Int = 0, mrbwb: Int = 52428) 
 extends Config((_, _, _) => {
   case BRUKey => {
     Some(BRUParams(
       address = address, 
-      nDomains = nDomains, 
+      nRCID = nRCID,
+      nMCID = nMCID, 
       withMonitor = withMonitor,
 
       ver = ver,
@@ -384,12 +416,3 @@ extends Config((_, _, _) => {
     ))
   }
 })
-
-// MMIO map examples (nDomains=4, n=4, base 0x00, 8-byte stride):
-// Core regs:
-//   enBRUGlobal=0x00, countInstrFetch=0x08, periodLen=0x10
-//   maxRead[0..3]=0x18..0x30, maxWrite[0..3]=0x38..0x50
-//   clientRegEnable=0x58, domainId[0..3]=0x60..0x78, perfEnable=0x80
-// Counters (withMonitor=true):
-//   nBanks=2: bankCountR[0..7]=0x88..0xC0, bankCountW[0..7]=0xC8..0x100
-//   nBanks=4: bankCountR[0..15]=0x88..0x100, bankCountW[0..15]=0x108..0x180
